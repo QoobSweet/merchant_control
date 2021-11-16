51966919e94e9497f61e11435c63e195b4f072ea
Kevin <QoobSweet@gmail.com>
2021-11-16 14:49:28 -0500

    seeded State and Country in database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Country;
+use App\Models\State;
 use App\Models\StatusCollection;
 use App\Models\User;
 use App\Models\Team;
@@ -11,6 +13,7 @@ use App\Models\Lead;
 use App\Models\Section;
 use Illuminate\Database\Seeder;
 use PhpParser\ErrorHandler\Collecting;
+use PHPUnit\Framework\Constraint\Count;
 
 class DatabaseSeeder extends Seeder
 {
@@ -25,8 +28,75 @@ class DatabaseSeeder extends Seeder
         Status::truncate();
         Board::truncate();
 
+        $unitedStates = Country::create([
+            'long_name' => 'United States',
+            'short_name' => 'US'
+        ]);
+        $states = [
+            "AK" => "Alaska",
+            "AL" => "Alabama",
+            "AR" => "Arkansas",
+            "AS" => "American Samoa",
+            "AZ" => "Arizona",
+            "CA" => "California",
+            "CO" => "Colorado",
+            "CT" => "Connecticut",
+            "DC" => "District of Columbia",
+            "DE" => "Delaware",
+            "FL" => "Florida",
+            "GA" => "Georgia",
+            "GU" => "Guam",
+            "HI" => "Hawaii",
+            "IA" => "Iowa",
+            "ID" => "Idaho",
+            "IL" => "Illinois",
+            "IN" => "Indiana",
+            "KS" => "Kansas",
+            "KY" => "Kentucky",
+            "LA" => "Louisiana",
+            "MA" => "Massachusetts",
+            "MD" => "Maryland",
+            "ME" => "Maine",
+            "MI" => "Michigan",
+            "MN" => "Minnesota",
+            "MO" => "Missouri",
+            "MS" => "Mississippi",
+            "MT" => "Montana",
+            "NC" => "North Carolina",
+            "ND" => "North Dakota",
+            "NE" => "Nebraska",
+            "NH" => "New Hampshire",
+            "NJ" => "New Jersey",
+            "NM" => "New Mexico",
+            "NV" => "Nevada",
+            "NY" => "New York",
+            "OH" => "Ohio",
+            "OK" => "Oklahoma",
+            "OR" => "Oregon",
+            "PA" => "Pennsylvania",
+            "PR" => "Puerto Rico",
+            "RI" => "Rhode Island",
+            "SC" => "South Carolina",
+            "SD" => "South Dakota",
+            "TN" => "Tennessee",
+            "TX" => "Texas",
+            "UT" => "Utah",
+            "VA" => "Virginia",
+            "VI" => "Virgin Islands",
+            "VT" => "Vermont",
+            "WA" => "Washington",
+            "WI" => "Wisconsin",
+            "WV" => "West Virginia",
+            "WY" => "Wyoming"
+        ];
 
-
+        foreach ($states as $abbr => $longName) {
+            State::create([
+                'country_id' => $unitedStates->id,
+                'long_name' => $longName,
+                'short_name' => $abbr
+            ]);
+        }
 
         // create Admin User
         $user = new User();
